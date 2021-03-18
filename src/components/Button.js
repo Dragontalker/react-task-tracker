332e063f51c8f5eba0612400c9f94d40636c00ec
Richard <richard.yang.tong@gmail.com>
2021-03-18 17:55:09 -0400

    Passed in onclick functions.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,28 @@
+import PropTypes from 'prop-types';
+
 const Button = (props) => {
 
+
     return (
-        <button className='btn' style={{backgroundColor: props.color}}>
+        <button 
+            onClick={props.onClick}
+            className='btn' 
+            style={{backgroundColor: props.color}}
+        >
             {props.text}
         </button>
     )
+};
+
+Button.defaultProps = {
+    color: 'steelblue',
+    text: 'Button'
+};
+
+Button.protoTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func.isRequired
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,17 @@ import PropTypes from 'prop-types';
 import Button from './Button'
 
 const Header = (props) => {
+    const onClick = () => {
+        console.log('Click');
+    }
 
     return (
         <header className='header'>
             <h1>{props.title}</h1>
-            <Button color='green' text='hello1'/>
+            <Button color='green' text='hello1' onClick={onClick}/>
             <Button color='blue' text='hello2'/>
             <Button color='red' text='hello3'/>
+            <Button />
         </header>
     )
 };
